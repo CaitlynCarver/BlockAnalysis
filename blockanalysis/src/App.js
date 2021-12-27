27f791deea9953a78bb93383cba0b0baaa220bd4
CaitlynCarver <52757675+CaitlynCarver@users.noreply.github.com>
2021-12-27 12:20:39 -0500

    first push

---
+++ blockanalysis/src/App.js
@@ -0,0 +1,61 @@
+import './App.css';
+import SideNav, { NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import React, { useState } from "react"
+import Web3 from web3
+
+// Be sure to include styles at some point, probably during your bootstraping
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+
+
+function App() {
+  const [fname, setFname] = useState("")
+
+  const handleChange = e => {
+    setFname(e.target.value)
+  }
+
+  return (
+    <div className="App">
+        <SideNav id="navcolor">
+          <SideNav.Toggle />
+            <SideNav.Nav defaultSelected="home">
+              <NavItem eventKey="home">
+                  <NavIcon>
+                      <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                  </NavIcon>
+                  <NavText>
+                      Home
+                  </NavText>
+              </NavItem>
+          
+          <NavItem eventKey="charts">
+            <NavIcon>
+                <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Charts
+            </NavText>
+          </NavItem>
+          </SideNav.Nav>
+        </SideNav>
+
+      <div>
+
+      <header className="App-header">
+        Hello There
+        <form>
+          <label>
+            Crypto Address:{" "}
+            <input type="text" value={fname} onChange={handleChange} />
+          </label>
+        </form>
+        <h5>Crypto Address: {fname}</h5>
+      </header>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
